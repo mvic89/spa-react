9050a108625897da4758de1458617c0a765ea613
Victor Furustubbe <mantykanto2@gmail.com>
2025-04-22 19:25:45 +0200

    connected header and nav

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import { useState} from 'react'
 import Home from './pages/Home'
 import Games from './pages/Games'
 import Consoles from './pages/Consoles'
+import { getImageUrl } from './utils/functions'
+
+
 function App() {
   const [currentPage, setCurrentPage] = useState(null)
   return (
     <>
-
-      <nav className='nav'>
-        <li onClick={() => setCurrentPage(null)}>Home</li>
-        <li onClick={() => setCurrentPage(<Consoles/>)}>Console</li>
-        <li onClick={() => setCurrentPage(<Games/>)}>Games</li>
-      </nav>
+      <div className='header'>
+        <img className='logotype' src={getImageUrl('sega.png')} />
+        <nav className='nav'>
+          <li onClick={() => setCurrentPage(null)}>Home</li>
+          <li onClick={() => setCurrentPage(<Consoles/>)}>Console</li>
+          <li onClick={() => setCurrentPage(<Games/>)}>Games</li>
+        </nav>
+      </div>
       
 
       {currentPage === null ? <Home/> : currentPage}
