ae80721c7a8c92ca865a490ba6e8b197ecf9faab
Victor Furustubbe <mantykanto2@gmail.com>
2025-04-25 21:18:55 +0200

    added content to main content section

--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -1,9 +1,14 @@
 import styles from './main-content.module.css'
+import { getImageUrl } from '../../utils/functions'
 
 const MainContent = () => {
     return (
+        <div className={styles.mainContainer}>
+        <img className={styles.contentImage} src={getImageUrl('megadrive.png')} />
         <div>
-            Main Content
+            <h1>Sega MegaDrive</h1>
+            <p>This is a page dedicated to one of my favorite consoles growing up as a kid. It contains some background history about the SEGA Megadrive as well as a list of 10 bad ass games that no one should have missed out of. </p>
+        </div>
         </div>
     )
 }
