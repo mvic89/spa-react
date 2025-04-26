2138b3ee96d2f8d0bf4f2fef589f82ef24549671
Victor Furustubbe <mantykanto2@gmail.com>
2025-04-26 20:15:34 +0200

    fixing the hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -2,12 +2,10 @@ import styles from './hero.module.css'
 
 const Hero = ({title='Welcome to the Home Page!', subtitle='The page that pays homage to everything SEGA!'}) => {
     return (
-        <section className={styles.heroContainer}>
-            <div>
-                <h1>{title}</h1>
-                <p>{subtitle}</p>
-            </div>
-        </section>
+        <div className={styles.divContainer}>
+            <h1>{title}</h1>
+            <p>{subtitle}</p>
+        </div>
     )
 }
 
--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -1,14 +1,12 @@
 import styles from './main-content.module.css'
 import { getImageUrl } from '../../utils/functions'
 
-const MainContent = () => {
+const MainContent = ({textTitle, paragraphText}) => {
     return (
         <div className={styles.mainContainer}>
-        <img className={styles.contentImage} src={getImageUrl('megadrive.png')} />
-        <div>
-            <h1>Sega MegaDrive</h1>
-            <p>This is a page dedicated to one of my favorite consoles growing up as a kid. It contains some background history about the SEGA Megadrive as well as a list of 10 bad ass games that no one should have missed out of. </p>
-        </div>
+            <img className={styles.contentImage} src={getImageUrl('megadrive.png')} />
+            <h1>{textTitle}</h1>
+            <p>{paragraphText}</p>
         </div>
     )
 }
