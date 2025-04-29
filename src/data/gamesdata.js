66bf8dd6d6f84e9403cc79b0fd2d2b4763656ab6
Victor Furustubbe <mantykanto2@gmail.com>
2025-04-29 15:54:14 +0200

    added games data

---
+++ src/data/gamesdata.js
@@ -0,0 +1,66 @@
+[
+    {
+        title: `Disney's Aladdin`,
+        developer: 'Developed by: Virgin Games, Inc.',
+        genre: 'Genre: Action, Platformer',
+        release: 'Release Date: October 19, 1993',
+        players: 'Max Players: 1',
+        playmode: 'Play mode: Single Player'
+    },
+    {
+        title: `Earthworm Jim`,
+        developer: 'Developed by: Shiny Entertainment',
+        genre: 'Genre: Action, Platformer',
+        release: 'Realease Date: August 2, 1994',
+        players: 'Max Players: 1',
+        playmode: 'Play mode: Single Player'
+    },
+    {
+        title: `Cool Spot`,
+        developer: 'Developed by: Virgin Games',
+        genre: 'Genre: Action, Platformer',
+        release: 'Realease Date: May, 1993',
+        players: 'Max Players: 1',
+        playmode: 'Play mode: Single Player'
+    },
+    {
+        title: `Michael Jackson's Moonwalker`,
+        developer: 'Developed by: SEGA Enterprises Ltd.',
+        genre: 'Genre: Action, Platformer',
+        release: 'Realease Date: August 24, 1990',
+        players: 'Max Players: 2',
+        playmode: 'Play mode: Single Player, Versus (Alternating)'
+    },
+    {
+        title: `Thunder Force IV`,
+        developer: 'Developed by: Technosoft Co., Ltd.',
+        genre: `Genre: Shoot 'Em Up`,
+        release: 'Realease Date: July 24, 1992',
+        players: 'Max Players: 1',
+        playmode: 'Play mode: Single Player'
+    },
+    {
+        title: `Phantasy Star IV`,
+        developer: 'Developed by: Sega AM7 R&D Division',
+        genre: `Genre: RPG (Turn-based)`,
+        release: 'Realease Date: November, 1993',
+        players: 'Max Players: 1',
+        playmode: 'Play mode: Single Player'
+    },
+    {
+        title: `Sparkster`,
+        developer: 'Developed by: Konami Co., Ltd.',
+        genre: 'Genre: Action, Platformer',
+        release: 'Realease Date: September, 1994',
+        players: 'Max Players: 1',
+        playmode: 'Play mode: Single Player'
+    },
+    {
+        title: `Streets of Rage`,
+        developer: 'Developed by: SEGA Enterprises Ltd.',
+        genre: `Genre: Action, Beat 'em Up`,
+        release: 'Realease Date: September 18, 1991',
+        players: 'Max Players: 2',
+        playmode: 'Play mode: Co-op (Simultaneous)'
+    },
+]
