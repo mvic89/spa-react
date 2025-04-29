3e298ca30264ade65b6642d16371948e4999bb70
Victor Furustubbe <mantykanto2@gmail.com>
2025-04-29 16:59:48 +0200

    initiating games branch

---
+++ src/components/GamesContent/index.jsx
@@ -0,0 +1,9 @@
+import styles from './games-content.module.css'
+
+const GamesContent = () => {
+    return (
+        <div>Games Content</div>
+    )
+}
+
+export default GamesContent
--- src/pages/Games/index.jsx
+++ src/pages/Games/index.jsx
@@ -1,10 +1,12 @@
 import Footer from "../../components/Footer"
+import GamesContent from "../../components/GamesContent"
 import Hero from "../../components/Hero"
 
 const Games = () => {
     return (
         <>
         <Hero title='Welcome to the Games Page' subtitle='Information about SEGA Games!'/>
+        <GamesContent/>
         <Footer/>
         </>
     )
