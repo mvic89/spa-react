cdbed71af81d56dd08cc28d4073bbacaf9751eaf
Victor Furustubbe <mantykanto2@gmail.com>
2025-05-01 13:23:37 +0200

    added data

--- src/data/gamesdata.js
+++ src/data/gamesdata.js
@@ -1,6 +1,7 @@
 [
     {
         title: `Disney's Aladdin`,
+        description: `The player controls Aladdin, who must make his way through several levels based on locations from the movie: from the streets and rooftops of Agrabah, the Cave of Wonders and the Sultan's dungeon to the final confrontation in Grand Vizier Jafar's palace.`,
         developer: 'Developed by: Virgin Games, Inc.',
         genre: 'Genre: Action, Platformer',
         release: 'Release Date: October 19, 1993',
@@ -9,6 +10,7 @@
     },
     {
         title: `Earthworm Jim`,
+        description: `Earthworm Jim is a 2D side-scrolling platformer with action elements where the player takes control of Jim, a worm wearing a spacesuit. Jim can shoot enemies with his gun or use his body (the spacesuit's "head") in a variety of ways to overcome obstacles,`, 
         developer: 'Developed by: Shiny Entertainment',
         genre: 'Genre: Action, Platformer',
         release: 'Realease Date: August 2, 1994',
@@ -17,6 +19,7 @@
     },
     {
         title: `Cool Spot`,
+        description: `Cool Spot is a colorful 2D platform game featuring the 7-Up mascot as the main protagonist. The game objective is fairly simple and straightforward. The player character has to collect enough number of cool points throughout each level in order to find and rescue his captured uncool Spot buddies and complete the game.`,
         developer: 'Developed by: Virgin Games',
         genre: 'Genre: Action, Platformer',
         release: 'Realease Date: May, 1993',
@@ -24,15 +27,8 @@
         playmode: 'Play mode: Single Player'
     },
     {
-        title: `Michael Jackson's Moonwalker`,
-        developer: 'Developed by: SEGA Enterprises Ltd.',
-        genre: 'Genre: Action, Platformer',
-        release: 'Realease Date: August 24, 1990',
-        players: 'Max Players: 2',
-        playmode: 'Play mode: Single Player, Versus (Alternating)'
-    },
-    {
         title: `Thunder Force IV`,
+        description: `Quest for the Darkstar (Thunder Force IV in Japan and Europe) is, like its predecessor, a side-scrolling shoot-em-up. The gameplay system is similar to that of the previous game, upgradeable default weapons as well as additional ones that can be collected throughout the stages.`,
         developer: 'Developed by: Technosoft Co., Ltd.',
         genre: `Genre: Shoot 'Em Up`,
         release: 'Realease Date: July 24, 1992',
@@ -41,6 +37,7 @@
     },
     {
         title: `Phantasy Star IV`,
+        description: `Phantasy Star IV is a Japanese-style role-playing game with top-down world map exploration, random enemy encounters, and turn-based combat. The gameplay of the fourth entry in the series is similar to that of the second installment, sharing notable features such as the ability to equip two weapons at once, most of the "techniques" (equivalent to magic spells in comparable games), etc. `,
         developer: 'Developed by: Sega AM7 R&D Division',
         genre: `Genre: RPG (Turn-based)`,
         release: 'Realease Date: November, 1993',
@@ -49,6 +46,7 @@
     },
     {
         title: `Sparkster`,
+        description: `The game consists of six levels with 2D side-scrolling platformer gameplay. The opossum Sparkster can move around, jump and attack with his sword. Using his rocket pack, he can also perform stronger attacks and burst through the air in different directions.`,
         developer: 'Developed by: Konami Co., Ltd.',
         genre: 'Genre: Action, Platformer',
         release: 'Realease Date: September, 1994',
@@ -57,10 +55,20 @@
     },
     {
         title: `Streets of Rage`,
+        description: `Streets of Rage, Sega's answer to Final Fight, follows the story of three young police officers (Adam Hunter, Axel Stone and Blaze Fielding) in a city controlled by a criminal syndicate led by a "Mr. X" where crime is rampant, which leads the three heroes to make a pact to leave the force and topple the syndicate by themselves.`,
         developer: 'Developed by: SEGA Enterprises Ltd.',
         genre: `Genre: Action, Beat 'em Up`,
         release: 'Realease Date: September 18, 1991',
         players: 'Max Players: 2',
         playmode: 'Play mode: Co-op (Simultaneous)'
+    },
+    {
+        title: `Sonic The Hedgehog 2`,
+        description: `Sonic the Hedgehog 2 is a side-scrolling platformer based around speed, and the sequel to Sonic the Hedgehog. Like the original, players run through different worlds called "zones", which each have their own specific theme.`,
+        developer: 'Developed by: Sega Technical Institute, Sonic Team',
+        genre: `Genre: Action, Action, Platformer`,
+        release: 'Realease Date: November 21, 1992',
+        players: 'Max Players: 2',
+        playmode: 'Play mode: Versus (Simultaneous)'
     },
 ]
