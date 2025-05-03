3bed0edab5f3339182f9c6e90275ba49ef30997f
Victor Furustubbe <mantykanto2@gmail.com>
2025-05-03 15:53:41 +0200

    added responsive games cards

--- src/components/GamesCard/index.jsx
+++ src/components/GamesCard/index.jsx
@@ -1,12 +1,30 @@
+import { useState } from 'react';
 import { getImageUrl } from '../../utils/functions'
 import styles from './games-card.module.css'
 
 
+
 const GamesCard = ({games}) => {
+    
+    const [showFullDescription, setShowFullDescription] = useState(false);
+
+    let description = games.description;
+
+    if (!showFullDescription ) {
+        description = description.substring(0, 90) + '...';
+    }
+
+
     return (
         <div className={styles.gamesDiv}> 
-            <h3>{games.title}</h3>
             <img className={styles.gamesImg} src={getImageUrl(games.images)} alt={`${games.images}`} />
+            <div className={styles.cardContent}>
+                <h3>{games.title}</h3>
+                <div>
+                    <p>{description}</p>
+                </div>
+                <button onClick={ () => setShowFullDescription((prevState) => !prevState)}>{showFullDescription ? 'Less' : 'More'}</button>
+            </div>
         </div>
     )
 }
