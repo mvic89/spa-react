74b0145b44038e9b00cf9b72473d1eb0baad48f8
Victor Furustubbe <mantykanto2@gmail.com>
2025-05-04 00:26:16 +0200

    cleaning up

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <>
         <Hero/>
-        <MainContent paragraphText='This is a page dedicated to one of my favorite consoles growing up as a kid. It contains some background history about the SEGA Megadrive as well as a list of 8 bad ass games that no one should have missed out on.'/>
+        <MainContent paragraphText='This is a page dedicated to one of my favorite consoles growing up as a kid. It contains some background history about the SEGA Mega Drive as well as a list of 8 badass games that no one should have missed out on.'/>
         <Footer/>
         </>
     )
