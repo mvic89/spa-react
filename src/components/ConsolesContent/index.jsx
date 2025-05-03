8e570595b02d7d836f1bed082132e4eec6dd240c
Victor Furustubbe <mantykanto2@gmail.com>
2025-05-03 23:50:28 +0200

    added console content

--- src/components/ConsolesContent/index.jsx
+++ src/components/ConsolesContent/index.jsx
@@ -1,14 +1,19 @@
 import { getImageUrl } from '../../utils/functions'
 import styles from './consoles-content.module.css'
 
-const ConsoleContent = ({consolesParagraph}) => {
+const ConsoleContent = ({conHead,conPar1,conPar2,conPar3}) => {
     return (
         <div className={styles.consoleContainer}>
             <div className={styles.consoleImage}>
                 <img src={getImageUrl('sega-megadrive.jpg')} alt="Sega Megadrive"/>
                 <img src={getImageUrl('sega-genesis.jpg')} alt="Sega Genesis"/>
             </div>
-            <p className={styles.textStyling}>{consolesParagraph}</p>
+            <div className={styles.consoleText}>
+                <h1>{conHead}</h1>
+                <p className={styles.textStyling}>{conPar1}</p>
+                <p className={styles.textStyling}>{conPar2}</p>
+                <p className={styles.textStyling}>{conPar3}</p>
+            </div>
         </div>
     )
 }
--- src/pages/Consoles/index.jsx
+++ src/pages/Consoles/index.jsx
@@ -6,7 +6,12 @@ const Consoles = () => {
     return (
         <>
         <Hero title='Welcome to the Consoles Page!' subtitle='Information about the SEGA Consoles '/>
-        <ConsoleContent/>
+        <ConsoleContent 
+            conHead='History'
+            conPar1='The SEGA Mega Drive/Genesis (as known in the US) was a revolutionary 16-Bit console at the time and is the system attributed to the historical success the company enjoyed and as such is an essential part of gaming history.'
+            conPar2='It was the console that broke the monopolies Nintendo had on the console industry by inspiring the first great console war that took place from boardrooms to playgrounds winning SEGA a substantial market share bringing them from an $813 million company in 1989 to a $3.6 billion conglomerate by 1993.'
+            conPar3='This battle, most fiercely fought in North America, was not only good for profits but it was the catalyst for taking the image of gaming away from the player in seclusion to the masses where it is today, and oh yes, introduced us to the to the phenomenon that is Sonic The Hedgehog.'
+        />
         <Footer/>
         </>
     )
