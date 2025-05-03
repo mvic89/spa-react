086e12ac58965000c7fcd86dd3c45695599f5cb5
Victor Furustubbe <mantykanto2@gmail.com>
2025-05-03 22:26:32 +0200

    added console images

--- src/components/ConsolesContent/index.jsx
+++ src/components/ConsolesContent/index.jsx
@@ -1,9 +1,14 @@
+import { getImageUrl } from '../../utils/functions'
 import styles from './consoles-content.module.css'
 
-const ConsoleContent = () => {
+const ConsoleContent = ({consolesParagraph}) => {
     return (
-        <div>
-            Console Content!
+        <div className={styles.consoleContainer}>
+            <div className={styles.consoleImage}>
+                <img src={getImageUrl('sega-megadrive.jpg')} alt="Sega Megadrive"/>
+                <img src={getImageUrl('sega-genesis.jpg')} alt="Sega Genesis"/>
+            </div>
+            <p className={styles.textStyling}>{consolesParagraph}</p>
         </div>
     )
 }
