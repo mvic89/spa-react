0c6581d3a3603eff72eaa0c28c9a6f9dcf853b88
Victor Furustubbe <mantykanto2@gmail.com>
2025-05-04 00:08:27 +0200

    added responsivness to console content

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <>
         <Hero/>
-        <MainContent paragraphText='This is a page dedicated to one of my favorite consoles growing up as a kid. It contains some background history about the SEGA Megadrive as well as a list of 10 bad ass games that no one should have missed out of.'/>
+        <MainContent paragraphText='This is a page dedicated to one of my favorite consoles growing up as a kid. It contains some background history about the SEGA Megadrive as well as a list of 8 bad ass games that no one should have missed out on.'/>
         <Footer/>
         </>
     )
